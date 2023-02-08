dbbd552efd8d3c64a6e111cd5e16d485ffc015be
Milan Vaško <m.vasko2@gmail.com>
2023-02-08 08:17:54 +0100

    feat: Svelte hello world

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  let count = 1;
+
+  function increment(): void {
+    count += 1;
+  }
+
+  function decrement(): void {
+    count -= 1;
+  }
+</script>
+
+<h1>Welcome to SvelteKit!</h1>
+<p>The current count is {count}.</p>
+<button on:click={increment}>Increment</button>
+<button on:click={decrement}>Decrement</button>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
